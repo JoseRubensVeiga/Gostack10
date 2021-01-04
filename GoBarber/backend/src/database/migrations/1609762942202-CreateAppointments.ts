f7a5201ec89704b39507872ee8b33d6ebb751974
José <jose.rubens.veiga@gmail.com>
2021-01-04 09:46:06 -0300

    incluindo o typeorm no backend do gobarber

---
+++ GoBarber/backend/src/database/migrations/1609762942202-CreateAppointments.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateAppointments1609762942202 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            isUnique: true,
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appoitments');
+  }
+}
